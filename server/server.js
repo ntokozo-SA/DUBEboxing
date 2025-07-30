fe360e049fa70cd64ba5346b4344f1add408de09
ntokozo-SA <Ntokozobuthelezi205@gmail.com>
2025-07-30 13:02:32 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -174,10 +174,12 @@ app.put('/api/settings', auth, (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
