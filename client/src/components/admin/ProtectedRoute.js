4ffb486006933338f928e597d81f6abd2f7c8da8
ntokozo-SA <Ntokozobuthelezi205@gmail.com>
2025-07-31 18:22:12 +0200

    Update ProtectedRoute.js

--- client/src/components/admin/ProtectedRoute.js
+++ client/src/components/admin/ProtectedRoute.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+// client/src/components/admin/ProtectedRoute.js
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem('adminToken');
-  
-  if (!token) {
-    return <Navigate to="/admin/login" replace />;
-  }
-
   return children;
 };
 
-export default ProtectedRoute; 
+export default ProtectedRoute;
